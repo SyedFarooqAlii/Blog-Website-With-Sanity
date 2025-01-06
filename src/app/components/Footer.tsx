6e85e524f2b6be602509e752e256ca0ee8360eb8
SyedFarooqAlii <syedfarooqali15@gmail.com>
2025-01-06 15:31:08 +0500

    update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -42,10 +42,10 @@ const Footer = () => {
         key={index}
         className="bg-white shadow-lg rounded-lg p-6 hover:scale-105 transition-transform duration-300"
       >
-        <h3 className="text-sm font-semibold text-gray-500 mb-2">
+        <h3 className="text-sm font-semibold text-[#ea1b48] mb-2">
           Blog Articles
         </h3>
-        <h2 className="text-[1.2rem] font-bold text-gray-800 mb-4">{article.title}</h2>
+        <h2 className="text-[1.2rem] font-bold hover:text-[#ea1b48] text-gray-800 mb-4">{article.title}</h2>
         <p className="text-gray-600">{article.description}</p>
         <hr className="mt-4 border-gray-200" />
       </div>
